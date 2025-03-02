3fb92c76a61e322daacd60915ed3fb09c3385a49
jesus <jesusrguez970@gmail.com>
2025-03-01 19:29:45 -0600

    Inbox page (static)

---
+++ app/components/inbox/conversation.tsx
@@ -0,0 +1,10 @@
+const Conversation = () => {
+    return(
+        <div className="px-6 py-6 border border-gray-600 rounded-xl">
+            <p className="mb-6 tetx-xl">Juanito Alimaña</p>
+
+            <p className="text-airbnb-dark">A chismear</p>
+        </div>
+    );
+}
+export default Conversation;
